b1ae86f216c3b7891858b4699eb75ed9ea9766bb
UddayanSharma6395 <uddayansharma6@gmail.com>
2024-04-13 12:36:03 +0530

    added some js

---
+++ myJSFile.js
@@ -0,0 +1,11 @@
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`,
+        imageurl: document.getElementById("imageURL").value,
+        tasktitle: document.getElementById("taskTitle").value,
+        tasktype: document.getElementById("taskType").value,
+        taskdescription: document.getElementById("taskDescription").value
+
+    };
+    console.log(taskData);
+};
